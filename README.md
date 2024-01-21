8a51a6f3fdc7f770d46783036ac1965e299ad910
toidicakhia <toidicakhia1234@gmail.com>
2024-01-21 11:36:52 +0700

    test

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # ViaVersionPlugin
 This is a plugin for MinusBounce that it helps you play on any version to 1.20.1
 
-# Information of MinusBounce
+### Information of MinusBounce
 - Website: https://minusbounce.lol
 - Discord: https://discord.minusbounce.lol
 
-# Installation
+### Installation
 - Step 1: Make sure that MinusBounce was installed in mods folder. If it wasn't installed, you can download in [here](https://minusbounce.lol/download)
 - Step 2: Download ViaVersionPlugin from [here](https://github.com/MinusMC/ViaVersionPlugin/releases)
 - Step 3: Extract, and copy jar file to mods folder
 - Step 4: Launch!
 
-# Credits
+### Credits
 - Fyxar (for old 1.17 transaction fix)
 - [ViaVersion](https://github.com/ViaVersion/ViaVersion)
 - FlorianMichael (for [ViaMCP](https://github.com/ViaVersionMCP/ViaMCP) and transaction fix)
--- src/main/java/net/minusmc/viaversionplugin/injection/forge/mixins/network/MixinNetHandlerPlayClient.java
+++ src/main/java/net/minusmc/viaversionplugin/injection/forge/mixins/network/MixinNetHandlerPlayClient.java
@@ -1,24 +1,19 @@
 package net.minusmc.viaversionplugin.injection.forge.mixins.network;
 
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.network.PacketThreadUtil;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.vialoadingbase.ViaLoadingBase;
 import net.minecraft.network.play.client.C0FPacketConfirmTransaction;
 import net.minecraft.network.play.server.S32PacketConfirmTransaction;
 import net.minecraft.network.Packet;
-import net.minecraft.inventory.Container;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = NetHandlerPlayClient.class, priority = 1001)
 public class MixinNetHandlerPlayClient {
-
-	@Shadow
-	private Minecraft gameController;
 
     @Shadow
     public void addToSendQueue(Packet p_addToSendQueue_1_) {}
@@ -28,32 +23,11 @@ public class MixinNetHandlerPlayClient {
      * @reason 1.17+ transaction fix
      */
 
-	@Overwrite
-	public void handleConfirmTransaction(S32PacketConfirmTransaction packetIn) {
-        PacketThreadUtil.checkThreadAndEnqueue(packetIn, (NetHandlerPlayClient) (Object) this, this.gameController);
-        
+    @Inject(method="handleConfirmTransaction", at=@At(value="INVOKE", target="Lnet/minecraft/network/PacketThreadUtil;checkThreadAndEnqueue(Lnet/minecraft/network/Packet;Lnet/minecraft/network/INetHandler;Lnet/minecraft/util/IThreadListener;)V", shift=At.Shift.AFTER), cancellable=true)
+    private void handleConfirmTransaction(S32PacketConfirmTransaction packetIn, CallbackInfo callbackInfo) {
         if (ViaLoadingBase.getInstance().getTargetVersion().isNewerThanOrEqualTo(ProtocolVersion.v1_17)) {
             this.addToSendQueue(new C0FPacketConfirmTransaction(packetIn.getWindowId(), (short) 0, false));
-            return;
-        }
-
-        Container container = null;
-        EntityPlayer entityplayer = this.gameController.thePlayer;
-
-        if (entityplayer == null) return;
-
-        if (packetIn.getWindowId() == 0)
-        {
-            container = entityplayer.inventoryContainer;
-        }
-        else if (packetIn.getWindowId() == entityplayer.openContainer.windowId)
-        {
-            container = entityplayer.openContainer;
-        }
-
-        if (container != null && !packetIn.func_148888_e())
-        {
-            this.addToSendQueue(new C0FPacketConfirmTransaction(packetIn.getWindowId(), packetIn.getActionNumber(), true));
+            callbackInfo.cancel();
         }
     }
 }
